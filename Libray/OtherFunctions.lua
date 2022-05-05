2a63cc8e56025bdff967d4f1fda7c2a1d8c9ef75
358x3 <54907369+358x33@users.noreply.github.com>
2022-05-05 20:28:18 +0200

    Update OtherFunctions.lua

--- Libray/OtherFunctions.lua
+++ Libray/OtherFunctions.lua
@@ -1,19 +1,23 @@
 local OtherFunctions = {}
 
-function Functions.Create(Class,name,Parent,Value)
-  
-end
-function Functions.Update(name,Parent,Value)
-  
-end
-function Functions.Delete(name,Parent)
-  
-end
-function Functions.Print(Text)
-  
-end
-function Functions.CreateFile(Text)
-  
+local function OtherFunctions.getexploit()
+    local exploit =
+        (syn and not is_sirhurt_closure and not pebc_execute and "Synapse") or
+        (secure_load and "Sentinel") or
+        (is_sirhurt_closure and "Sirhurt") or
+        (pebc_execute and "ProtoSmasher") or
+        (KRNL_LOADED and "Krnl") or
+        (WrapGlobal and "WeAreDevs") or
+        (isvm and "Proxo") or
+        (shadow_env and "Shadow") or
+        (jit and "EasyExploits") or
+        (getreg()['CalamariLuaEnv'] and "Calamari") or
+        (unit and "") or
+        (IS_VIVA_LOADED and "VIVA") or
+        (IS_COCO_LOADED and "Coco") or
+        ("Undetectable")
+ 
+    return exploit
 end
 
 return OtherFunctions
