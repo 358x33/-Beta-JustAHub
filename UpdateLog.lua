24bab34b8e021fee89c56d162289fe1f2917af98
358x3 <54907369+358x33@users.noreply.github.com>
2022-05-05 22:23:48 +0200

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -2,7 +2,7 @@ local UpdateLog = {}
 
 local function UpdateLog.ShowLogs(Version)
    if Version then
-        return "Update Log"
+        return "Update Log: Added, fixed, removed, List Ui"
     end
 end
 return UpdateLog
