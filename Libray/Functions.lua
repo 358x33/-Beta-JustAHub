6aad667c66fd30cbcba6c5344760b86412629672
358x3 <54907369+358x33@users.noreply.github.com>
2022-05-05 18:54:23 +0200

    Create Functions.lua

---
+++ Libray/Functions.lua
@@ -0,0 +1,19 @@
+local Functions = {}
+
+function Functions.Create(Class,name,Parent,Value)
+  
+end
+function Functions.Update(name,Parent,Value)
+  
+end
+function Functions.Delete(name,Parent)
+  
+end
+function Functions.Print(Text)
+  
+end
+function Functions.CreateFile(Text)
+  
+end
+
+return Functions
