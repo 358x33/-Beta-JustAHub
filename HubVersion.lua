e4d17823ce77d38350e4ce52dc3357dc2941dd08
358x3 <54907369+358x33@users.noreply.github.com>
2022-05-05 22:22:13 +0200

    Update HubVersion.lua

--- HubVersion.lua
+++ HubVersion.lua
@@ -1,1 +1,13 @@
+local HubVersion = {}
 
+  local Version = "1.0.5"
+  local function HubVersion.GetScriptVersion(scriptVersion)
+  if scriptVersion == Version then
+  return "True"
+  else
+  loadstring(game:HttpGet("https://github.com/358x33/BetaJustAHub/blob/main/GameDetector.lua"))()
+  
+  end
+  end
+  
+return HubVersion
